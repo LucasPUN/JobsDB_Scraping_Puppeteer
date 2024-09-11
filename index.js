7791d50fa97405c088cfb8abc0d4bf924d985b98
Lucas <lucas_3_2_1@hotmail.com>
2024-09-11 11:22:28 +0800

    11/9

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import puppeteer from "puppeteer";
 import * as fs from "node:fs";
 import axios from "axios";
+import cron from "node-cron";
 
 const baseUrl = "http://localhost:3000";
 
 async function scrapeJobs() {
     const browser = await puppeteer.launch({
         headless: false,
-        defaultViewport: null
+        defaultViewport: null,
     });
 
     const page = await browser.newPage();
     const salaryRanges = ["17000-20000", "20000-25000"];
     // const salaryRanges = ["0-11000", "11000-14000", "14000-17000", "17000-20000", "20000-25000", "25000-30000", "30000-35000", "35000-40000", "40000-50000", "50000-60000", "60000-80000", "80000-120000", "120000-"];
-    const currentDate = new Date().toISOString().split('T')[0];
+    const currentDate = new Date().toISOString().split("T")[0];
 
     for (const salaryRange of salaryRanges) {
         let currentPage = 1;
@@ -28,7 +29,6 @@ async function scrapeJobs() {
         let nodeJsCount = 0;
         let mySqlCount = 0;
         let noSqlCount = 0;
-
 
         let jobCount = 0;
         let countItem;
@@ -46,12 +46,12 @@ async function scrapeJobs() {
             const jobCardData = await page.evaluate(() => {
                 const jobCards = document.querySelectorAll('[data-card-type="JobCard"]');
                 const jobData = [];
-                jobCards.forEach(card => {
+                jobCards.forEach((card) => {
                     const dataElements = card.querySelectorAll('[data-automation]');
                     const data = {};
                     data["id"] = card.dataset.jobId;
-                    dataElements.forEach(element => {
-                        const key = element.getAttribute('data-automation');
+                    dataElements.forEach((element) => {
+                        const key = element.getAttribute("data-automation");
                         const value = element.innerText.trim();
                         data[key] = value;
                     });
@@ -63,8 +63,6 @@ async function scrapeJobs() {
             const jobCards = await page.$$('[data-card-type="JobCard"]');
             const combinedData = [];
 
-
-
             for (const jobCard of jobCards) {
                 const jobTitleElement = await jobCard.$('[data-automation="jobTitle"]');
                 await jobTitleElement.click();
@@ -75,8 +73,8 @@ async function scrapeJobs() {
                     const jobDetail = document.querySelectorAll('[data-automation="jobAdDetails"]');
                     const jobDetailData = [];
                     const data = {};
-                    jobDetail.forEach(element => {
-                        const key = element.getAttribute('data-automation');
+                    jobDetail.forEach((element) => {
+                        const key = element.getAttribute("data-automation");
                         const value = element.innerText.trim();
                         data[key] = value;
                     });
@@ -84,25 +82,25 @@ async function scrapeJobs() {
                     return jobDetailData;
                 });
 
-                const jobDescription = detailData[0]?.['jobAdDetails'].toLowerCase().replace(/\s+/g, '');
+                const jobDescription = detailData[0]?.["jobAdDetails"].toLowerCase().replace(/\s+/g, "");
 
                 // 统计关键字出现次数
-                if (jobDescription.includes('java')) javaCount++;
-                if (jobDescription.includes('python')) pythonCount++;
-                if (jobDescription.includes('javascript')) javaScriptCount++;
-                if (jobDescription.includes('typescript')) typeScriptCount++;
-                if (jobDescription.includes('reactjs')) reactJsCount++;
-                if (jobDescription.includes('vuejs')) vueJsCount++;
-                if (jobDescription.includes('spring')) springCount++;
-                if (jobDescription.includes('nodejs')) nodeJsCount++;
-                if (jobDescription.includes('mysql')) mySqlCount++;
-                if (jobDescription.includes('nosql')) noSqlCount++;
+                if (jobDescription.includes("java")) javaCount++;
+                if (jobDescription.includes("python")) pythonCount++;
+                if (jobDescription.includes("javascript")) javaScriptCount++;
+                if (jobDescription.includes("typescript")) typeScriptCount++;
+                if (jobDescription.includes("reactjs")) reactJsCount++;
+                if (jobDescription.includes("vuejs")) vueJsCount++;
+                if (jobDescription.includes("spring")) springCount++;
+                if (jobDescription.includes("nodejs")) nodeJsCount++;
+                if (jobDescription.includes("mysql")) mySqlCount++;
+                if (jobDescription.includes("nosql")) noSqlCount++;
 
                 const combinedItem = {
                     date: currentDate,
                     salaryRange: salaryRange,
                     ...jobCardData.shift(),
-                    ...detailData.shift()
+                    ...detailData.shift(),
                 };
                 combinedData.push(combinedItem);
             }
@@ -122,21 +120,21 @@ async function scrapeJobs() {
                 NodeJS: nodeJsCount,
                 MySQL: mySqlCount,
                 NoSQL: noSqlCount,
-                date: currentDate
+                date: currentDate,
             };
             // console.log(`Java count: ${javaCount}`);
             // console.log(`JavaScript count: ${javaScriptCount}`);
             // console.log(`ReactJS count: ${reactJsCount}`);
 
             try {
-                console.log(`Calling API ${salaryRange}-page-${currentPage}....`)
+                console.log(`Calling API ${salaryRange}-page-${currentPage}....`);
                 await axios.post(
                     `${baseUrl}/v1/job-detail-list`,
                     JSON.stringify(combinedData),
                     {
                         headers: {
-                            'Content-Type': 'application/json'
-                        }
+                            "Content-Type": "application/json",
+                        },
                     }
                 );
                 console.log(`Successfully added ${salaryRange}-page-${currentPage} to server`);
@@ -144,27 +142,34 @@ async function scrapeJobs() {
                 console.error(err);
             }
             currentPage++;
-        } while (currentPage <= totalPages)
+        } while (currentPage <= totalPages);
 
         try {
-            console.log(`Calling count API `)
+            console.log(`Calling count API `);
             await axios.post(
                 `${baseUrl}/v1/job-count`,
                 JSON.stringify(countItem),
                 {
                     headers: {
-                        'Content-Type': 'application/json'
-                    }
+                        "Content-Type": "application/json",
+                    },
                 }
             );
             console.log(`Successfully added count`);
         } catch (err) {
             console.error(err);
         }
-
     }
 
     await browser.close();
 }
 
-await scrapeJobs();
+setInterval(() => {
+    console.log("脚本正在运行，保持活跃状态...");
+}, 10000);
+
+// 使用cron调度任务，每天中午12:00运行
+cron.schedule("09 17 * * *", () => {
+    console.log("在每天中午12点运行爬虫任务");
+    scrapeJobs();
+});
